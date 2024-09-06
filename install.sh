ff11407a2303ff9a706b2a5b056ded523d71aafb
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2024-09-06 16:20:39 +0630

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 apt update -y
-curl -o $PREFIX/bin/kl https://raw.githubusercontent.com/nyeinkokoaung404/Selector/main/Sel.sh
-chmod +x $PREFIX/bin/kl
+curl -o $PREFIX/bin/404 https://raw.githubusercontent.com/nyeinkokoaung404/Selector/main/Sel.sh
+chmod +x $PREFIX/bin/404
